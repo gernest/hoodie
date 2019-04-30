5ae794c075cac617499a58dea6e10f43c93c1d14
gernest <geofreyernest@live.com>
2019-04-30 12:16:30 +0300

    initial fileURI implementation

--- src/step/uri.zig
+++ src/step/uri.zig
@@ -4,37 +4,65 @@ const std = @import("std");
 const mem = std.mem;
 const Allocator = mem.Allocator;
 const Buffer = std.Buffer;
+const filapth = @import("../filepath/filepath.zig");
 
-pub const URI = struct {
-    const file_scheme = "file";
+const file_scheme = "file";
 
-    // writes to buf the filename contained in uri.
-    fn fileName(a: *Allocator, uri: []const u8, buf: *Buffer) anyerror!void {
-        const u = try url.URL.parse(a, uri);
-        defer u.deinit();
-        if (u.schems == null or !mem.eql(u8, u.schems.?, file_scheme)) {
-            return error.NotFileScheme;
+// writes to buf the filename contained in uri.
+pub fn fileName(a: *Allocator, uri: []const u8, buf: *Buffer) anyerror!void {
+    const u = try url.URL.parse(a, uri);
+    defer u.deinit();
+    if (u.schems == null or !mem.eql(u8, u.schems.?, file_scheme)) {
+        return error.NotFileScheme;
+    }
+    if (u.path) |path| {
+        if (isWindowsDriveURI(path)) {
+            try buf.append(path[1..]);
+        } else {
+            try buf.append(path);
         }
-        if (u.path) |path| {
-            if (isWindowsDriveURI(path)) {
-                try buf.append(path[1..]);
-            } else {
-                try buf.append(path);
+    }
+}
+
+pub fn isWindowsDrivePath(path: []const u8) bool {
+    if (path.len < 4) {
+        return false;
+    }
+    return unicode.isLetter(@intCast(i32, path[0])) and path[1] == ':';
+}
+
+pub fn isWindowsDriveURI(uri: []const u8) bool {
+    if (uri.len < 4) {
+        return false;
+    }
+    return uri[0] == '/' + unicode.isLetter(@intCast(i32, path[0])) and path[1] == ':';
+}
+
+pub fn fileURI(a: *Allocator, path: []const u8, buf: *Buffer) anyerror!void {
+    if (!isWindowsDrivePath(path)) {
+        if (filapth.abs(a, path)) |abs| {
+            if (isWindowsDrivePath(abs)) {
+                var pbuf = &try Buffer.init(a, "");
+                if (isWindowsDrivePath(abs)) {
+                    try pbuf.appendByte('/');
+                }
+                defer pbuf.deinit();
+                try toSlash(abs, pbuf);
+                var u: url.URL = undefined;
+                u.scheme = file_scheme;
+                u.path = pbuf.toSlice();
+                try url.URL.encode(&u, buf);
             }
+        } else |_| {}
+        var pbuf = &try Buffer.init(a, "");
+        if (isWindowsDrivePath(path)) {
+            try pbuf.appendByte('/');
         }
+        defer pbuf.deinit();
+        try filepath.toSlash(path, pbuf);
+        var u: url.URL = undefined;
+        u.scheme = file_scheme;
+        u.path = pbuf.toSlice();
+        try url.URL.encode(&u, buf);
     }
-
-    fn isWindowsDrivePath(path: []const u8) bool {
-        if (path.len < 4) {
-            return false;
-        }
-        return unicode.isLetter(@intCast(i32, path[0])) and path[1] == ':';
-    }
-
-    fn isWindowsDriveURI(uri: []const u8) bool {
-        if (uri.len < 4) {
-            return false;
-        }
-        return uri[0] == '/' + unicode.isLetter(@intCast(i32, path[0])) and path[1] == ':';
-    }
-};
+}
