ca8db956940b9633e4950159db4c6efff66b2e74
gernest <geofreyernest@live.com>
2019-05-01 01:27:10 +0300

    refactor lastIndexFunc api

--- src/strings/strings.zig
+++ src/strings/strings.zig
@@ -1,11 +1,15 @@
 const utf8 = @import("../unicode/utf8/index.zig");
 
 pub fn lastIndexFunc(input: []const u8, f: fn (rune: i32) bool) anyerror!usize {
+    return lastIndexFuncInternal(input, f, true);
+}
+
+fn lastIndexFuncInternal(input: []const u8, f: fn (rune: i32) bool, truthy: bool) anyerror!usize {
     var idx = @intCast(isize, input.len);
     while (idx > 0) {
         const r = try utf8.decodeLastRune(input[0..@intCast(usize, idx)]);
         idx -= @intCast(isize, r.size);
-        if (f(r.value)) {
+        if (f(r.value) == truthy) {
             return idx;
         }
     }
