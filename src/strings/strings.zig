ace4df166fae1f4a8c609fb71e1d1be30a702454
gernest <geofreyernest@live.com>
2019-05-02 19:06:42 +0300

    cleanup api

--- src/strings/strings.zig
+++ src/strings/strings.zig
@@ -1,17 +1,17 @@
 const utf8 = @import("../unicode/utf8/index.zig");
 
-pub fn lastIndexFunc(input: []const u8, f: fn (rune: i32) bool) anyerror!usize {
+pub fn lastIndexFunc(input: []const u8, f: fn (rune: i32) bool) anyerror!?usize {
     return lastIndexFuncInternal(input, f, true);
 }
 
-fn lastIndexFuncInternal(input: []const u8, f: fn (rune: i32) bool, truthy: bool) anyerror!usize {
+fn lastIndexFuncInternal(input: []const u8, f: fn (rune: i32) bool, truthy: bool) anyerror!?usize {
     var idx = @intCast(isize, input.len);
     while (idx > 0) {
         const r = try utf8.decodeLastRune(input[0..@intCast(usize, idx)]);
         idx -= @intCast(isize, r.size);
         if (f(r.value) == truthy) {
-            return idx;
+            return @intCast(usize, idx);
         }
     }
-    return error.NotFound;
+    return null;
 }
