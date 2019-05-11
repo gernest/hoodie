2136e6c74aadbcf26577d5197dd4e50a882be27d
gernest <geofreyernest@live.com>
2019-05-11 13:44:00 +0300

    add encoding and decoding of ID

--- src/jsonrpc2/jsonrpc2.zig
+++ src/jsonrpc2/jsonrpc2.zig
@@ -5,6 +5,7 @@ const io = std.io;
 const event = std.event;
 const mem = std.mem;
 const warn = std.debug.warn;
+
 pub const json_rpc_version = "2.0";
 const content_length = "Content-Length";
 const default_message_size: usize = 8192;
@@ -41,164 +42,200 @@ pub const Error = struct {
     };
 };
 
-pub const RPC = struct {
-    pub const Request = struct {
-        jsonrpc: []const u8,
-        method: []const u8,
-        params: ?json.Value,
-        id: ?ID,
-    };
+pub const Request = struct {
+    jsonrpc: []const u8,
+    method: []const u8,
+    params: ?json.Value,
+    id: ?ID,
+};
 
-    pub const ID = union {
-        Name: []const u8,
-        Number: i64,
-    };
+pub const ID = union(enum) {
+    Name: []const u8,
+    Number: i64,
 
-    pub const Response = struct {
-        jsonrpc: []const u8,
-        result: ?json.Value,
-        err: ?Error,
-        id: ?ID,
-    };
-
-    // Context is a rpc call lifecycle object. Contains the rpc request and the
-    // reponse of serving the request.
-    pub const Context = struct {
-        request: *Request,
-        response: *Response,
-        arena: std.heap.ArenaAllocator,
-
-        // The requestParams might contain a json.Value object. The memory
-        // allocated on that object is in scope with this tree, so we keep this
-        // reference here to ensure all memory used in the duration of this
-        // context is properly freed when destorying the context.
-        tree: json.ValueTree,
-
-        pub fn init(a: *Allocator) !Context {
-            var self: Context = undefined;
-            self.arena = std.heap.ArenaAllocator.init(a);
-            var alloc = &self.arena.allocator;
-            self.request = alloc.create(Request);
-            self.response = alloc.create(Response);
-            self.tree = undefined;
-            return self;
+    pub fn encode(self: ID, a: *Allocator) json.Value {
+        switch (self) {
+            ID.Name => |v| {
+                return json.Value{ .String = v };
+            },
+            ID.Number => |v| {
+                return json.Value{ .Integer = v };
+            },
+            else => unreachable,
         }
+    }
 
-        pub fn deinit(self: *Context) void {
-            (&self.tree).deinit();
-            (&self.arena).deinit();
+    pub fn decode(self: *ID, value: json.Value) !void {
+        switch (value) {
+            json.Value.Integer => |v| {
+                self.* = ID{ .Number = v };
+            },
+            json.Value.String => |v| {
+                self.* = ID{ .Name = v };
+            },
+            else => return error.BadValue,
         }
+    }
+};
 
-        pub fn write(self: *Context, value: ?json.Value) void {
-            self.response.result = value;
+pub const Response = struct {
+    jsonrpc: []const u8,
+    result: ?json.Value,
+    err: ?Error,
+    id: ?ID,
+
+    pub fn encode(self: *Response, a: *Allocator) !json.Value {
+        var m = json.ObjectMap.init(a);
+        _ = try m.put("jsonrpc", json.Value{ .String = self.jsonrpc });
+        if (self.result != null) {
+            _ = try m.put("result", self.result.?);
         }
-
-        pub fn writeError(self: *Context, value: ?Error) void {
-            self.response.err = value;
+        if (self.err) |*v| {
+            _ = try m.put("error", try v.encode(a));
         }
-    };
+        if (self.id) |v| {
+            _ = try m.put("id", v.encode(a));
+        }
+    }
+};
 
-    pub const Handler = struct {
-        handleFn: fn (*Context) anyerror!void,
-    };
+// Context is a rpc call lifecycle object. Contains the rpc request and the
+// reponse of serving the request.
+pub const Context = struct {
+    request: *Request,
+    response: *Response,
+    arena: std.heap.ArenaAllocator,
 
-    pub const Conn = struct {
-        const ReadError = std.os.File.ReadError || error{};
-        const WriteError = std.os.File.WriteError || error{OutOfMemory} || error{};
+    // The requestParams might contain a json.Value object. The memory
+    // allocated on that object is in scope with this tree, so we keep this
+    // reference here to ensure all memory used in the duration of this
+    // context is properly freed when destorying the context.
+    tree: json.ValueTree,
 
-        const channel_buffer_size = 10;
+    pub fn init(a: *Allocator) !Context {
+        var self: Context = undefined;
+        self.arena = std.heap.ArenaAllocator.init(a);
+        var alloc = &self.arena.allocator;
+        self.request = alloc.create(Request);
+        self.response = alloc.create(Response);
+        self.tree = undefined;
+        return self;
+    }
 
-        pub const InStream = io.InStream(ReadError);
-        pub const OutStream = *io.OutStream(WriteError);
+    pub fn deinit(self: *Context) void {
+        (&self.tree).deinit();
+        (&self.arena).deinit();
+    }
 
+    pub fn write(self: *Context, value: ?json.Value) void {
+        self.response.result = value;
+    }
+
+    pub fn writeError(self: *Context, value: ?Error) void {
+        self.response.err = value;
+    }
+};
+
+pub const Handler = struct {
+    handleFn: fn (*Context) anyerror!void,
+};
+
+pub const Conn = struct {
+    const ReadError = std.os.File.ReadError || error{};
+    const WriteError = std.os.File.WriteError || error{OutOfMemory} || error{};
+
+    const channel_buffer_size = 10;
+
+    pub const InStream = io.InStream(ReadError);
+    pub const OutStream = *io.OutStream(WriteError);
+
+    a: *Allocator,
+    in: *InStream,
+    out: *OutStream,
+    requests_channel: *ContextChannel,
+    responses_channel: *ContextChannel,
+
+    const ContextChannel = event.Channel(*Context);
+
+    pub fn init(
         a: *Allocator,
-        in: *InStream,
-        out: *OutStream,
-        requests_channel: *ContextChannel,
-        responses_channel: *ContextChannel,
+        in_stream: *InStream.Stream,
+        out_stream: *OutStream.Stream,
+    ) Conn {
+        return Conn{
+            .a = a,
+            .in = in_stream,
+            .out = out_stream,
+            .requests_channel = undefined,
+            .responses_channel = undefined,
+        };
+    }
 
-        const ContextChannel = event.Channel(*Context);
+    pub fn serve(self: *Conn, loop: *event.Loop) anyerror!void {
+        self.requests_channel = try ContextChannel.create(loop, channel_buffer_size);
+        self.responses_channel = try ContextChannel.create(loop, channel_buffer_size);
+    }
 
-        pub fn init(
-            a: *Allocator,
-            in_stream: *InStream.Stream,
-            out_stream: *OutStream.Stream,
-        ) Conn {
-            return Conn{
-                .a = a,
-                .in = in_stream,
-                .out = out_stream,
-                .requests_channel = undefined,
-                .responses_channel = undefined,
-            };
+    async fn read(self: *Conn, loop: *event.Loop) !void {
+        var buf = &try std.Buffer.init(self.a, "");
+        defer buf.deinit();
+        try self.readRequestData(buf, self.in_stream);
+        var p = &json.Parser.init(self.a, true);
+        defer p.deinit();
+        while (true) {
+            var ctx = try self.a.create(Context);
+            try buf.resize(0);
+            p.reset();
+
+            var v = try p.parse(buf.toSlice());
+            switch (v.root) {
+                json.Value.Object => |*m| {
+                    var req: Request = undefined;
+                    ctx.tree = v;
+                    ctx.request.* = req;
+                },
+                else => return error.InvalidRPCPayload,
+            }
+            v.deinit();
         }
+    }
 
-        pub fn serve(self: *Conn, loop: *event.Loop) anyerror!void {
-            self.requests_channel = try ContextChannel.create(loop, channel_buffer_size);
-            self.responses_channel = try ContextChannel.create(loop, channel_buffer_size);
+    async fn write(self: *Conn, loop: *event.Loop) !void {
+        while (true) {
+            const ctx = await (try async self.responses_channel.get());
+
+            // cleanup all memory allocated during the lifetime of the
+            // context.
+            ctx.deinit();
         }
+    }
 
-        async fn read(self: *Conn, loop: *event.Loop) !void {
-            var buf = &try std.Buffer.init(self.a, "");
-            defer buf.deinit();
-            try self.readRequestData(buf, self.in_stream);
-            var p = &json.Parser.init(self.a, true);
-            defer p.deinit();
-            while (true) {
-                var ctx = try self.a.create(Context);
-                try buf.resize(0);
-                p.reset();
-
-                var v = try p.parse(buf.toSlice());
-                switch (v.root) {
-                    json.Value.Object => |*m| {
-                        var req: Request = undefined;
-                        ctx.tree = v;
-                        ctx.request.* = req;
-                    },
-                    else => return error.InvalidRPCPayload,
-                }
-                v.deinit();
+    pub fn readRequestData(buf: *std.Buffer, stream: var) !void {
+        var length: usize = 0;
+        while (true) {
+            try stream.readUntilDelimiterBuffer(
+                buf,
+                '\n',
+                default_message_size,
+            );
+            const line = trimSpace(buf.toSlice());
+            if (line.len == 0) {
+                break;
+            }
+            const colon = mem.indexOfScalar(u8, line, ':') orelse return error.InvalidHeader;
+            const name = line[0..colon];
+            const value = trimSpace(line[colon + 1 ..]);
+            if (mem.eql(u8, name, content_length)) {
+                length = try std.fmt.parseInt(usize, value, 10);
             }
         }
-
-        async fn write(self: *Conn, loop: *event.Loop) !void {
-            while (true) {
-                const ctx = await (try async self.responses_channel.get());
-
-                // cleanup all memory allocated during the lifetime of the
-                // context.
-                ctx.deinit();
-            }
+        if (length == 0) {
+            return error.MissingContentLengthHeader;
         }
-
-        pub fn readRequestData(buf: *std.Buffer, stream: var) !void {
-            var length: usize = 0;
-            while (true) {
-                try stream.readUntilDelimiterBuffer(
-                    buf,
-                    '\n',
-                    default_message_size,
-                );
-                const line = trimSpace(buf.toSlice());
-                if (line.len == 0) {
-                    break;
-                }
-                const colon = mem.indexOfScalar(u8, line, ':') orelse return error.InvalidHeader;
-                const name = line[0..colon];
-                const value = trimSpace(line[colon + 1 ..]);
-                if (mem.eql(u8, name, content_length)) {
-                    length = try std.fmt.parseInt(usize, value, 10);
-                }
-            }
-            if (length == 0) {
-                return error.MissingContentLengthHeader;
-            }
-            try buf.resize(length);
-            const n = try stream.read(buf.toSlice());
-            std.debug.assert(n == length);
-        }
-    };
+        try buf.resize(length);
+        const n = try stream.read(buf.toSlice());
+        std.debug.assert(n == length);
+    }
 };
 
 // simple adhoc way for removing starting and trailing whitespace.
--- src/jsonrpc2/jsonrpc2_test.zig
+++ src/jsonrpc2/jsonrpc2_test.zig
@@ -3,6 +3,10 @@ const io = std.io;
 const rpc = @import("jsonrpc2.zig");
 const warn = std.debug.warn;
 const testing = std.testing;
+const Buffer = std.Buffer;
+const json = std.json;
+const mem = std.mem;
+const dump = @import("../json/json.zig").dump;
 
 test "Conn.readRequestData" {
     const src = "Content-Length: 43\r\n\r\n{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"example\"}";
@@ -10,9 +14,56 @@ test "Conn.readRequestData" {
     var a = std.debug.global_allocator;
     var buf = &try std.Buffer.init(a, "");
     defer buf.deinit();
-    try rpc.RPC.Conn.readRequestData(buf, stream);
+    try rpc.Conn.readRequestData(buf, stream);
     const message =
         \\{"jsonrpc":"2.0","id":1,"method":"example"}
     ;
     testing.expect(buf.eql(message));
+}
+
+fn testEncode(buf: *Buffer, object: var, want: []const u8) !void {
+    var arena = std.heap.ArenaAllocator.init(buf.list.allocator);
+    defer arena.deinit();
+    var alloc = &arena.allocator;
+    try buf.resize(0);
+    var stream = &std.io.BufferOutStream.init(buf).stream;
+    var v = object.encode(alloc);
+    try dump(v, stream);
+    // warn("{}\n", buf.toSlice());
+    // warn("{}\n", buf.eql(want));
+    testing.expect(buf.eql(want));
+}
+
+fn testEncodeErr(buf: *Buffer, object: var, want: []const u8) !void {
+    var arena = std.heap.ArenaAllocator.init(buf.list.allocator);
+    defer arena.deinit();
+    var alloc = &arena.allocator;
+    try buf.resize(0);
+    var stream = &std.io.BufferOutStream.init(buf).stream;
+    var v = try object.encode(alloc);
+    try dump(v, stream);
+    // warn("\n{}\n", buf.toSlice());
+}
+
+test "rpc.ID" {
+    var a = std.debug.global_allocator;
+    var buf = &try std.Buffer.init(a, "");
+    defer buf.deinit();
+
+    var name_id = rpc.ID{ .Name = "id" };
+    try testEncode(buf, name_id,
+        \\"id"
+    );
+
+    var number_id = rpc.ID{ .Number = 1 };
+    try testEncode(buf, number_id,
+        \\1
+    );
+
+    var id: rpc.ID = undefined;
+    try (&id).decode(json.Value{ .Integer = 1 });
+    testing.expectEqual(id.Number, 1);
+
+    try (&id).decode(json.Value{ .String = "id" });
+    testing.expect(mem.eql(u8, id.Name, "id"));
 }
