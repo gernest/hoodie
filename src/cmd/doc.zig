47eda6fde71743ac4d1d29708c58c04627dcfbfa
gernest <geofreyernest@live.com>
2019-06-08 12:23:37 +0300

    initial work on docs

--- src/cmd/doc.zig
+++ src/cmd/doc.zig
@@ -1,8 +1,22 @@
-const std = @import("std");
-const outline = @import("outline");
-
 const html = @import("docs/html.zig");
+const outline = @import("outline");
+const std = @import("std");
 
-const Doc = struct {
-    Title: []const u8,
+pub const File = struct {
+    path: []const u8,
+};
+
+pub const Package = struct {
+    path: []const u8,
+    name: ?[]const u8,
+};
+
+pub const Spot = enum {
+    ImportDecl,
+    ConstDecl,
+    TypeDecl,
+    VarDecl,
+    FuncDecl,
+    MethodDecl,
+    Use,
 };
