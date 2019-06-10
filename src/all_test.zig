a90c4e7681ad73ec817299d3c0f767c3fc9b2276
gernest <geofreyernest@live.com>
2019-06-10 22:43:24 +0300

    add strings test to build test

--- src/all_test.zig
+++ src/all_test.zig
@@ -2,4 +2,5 @@ test "hoodie" {
     _ = @import("unicode/all_test.zig");
     _ = @import("lsp/all_test.zig");
     _ = @import("time/time_test.zig");
+    _ = @import("strings/strings_test.zig");
 }
