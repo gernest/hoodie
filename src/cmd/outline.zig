e187dbc9df76df6b4074767148d8eeff7fbe6085
gernest <geofreyernest@live.com>
2019-06-18 21:32:10 +0300

    cleanup

--- src/cmd/outline.zig
+++ src/cmd/outline.zig
@@ -3,8 +3,6 @@ const outline = @import("../outline/outline.zig");
 const std = @import("std");
 
 const cli = @import("../flags/cli.zig");
-const Args = cli.Args;
-const Cli = cli.Cli;
 const Command = cli.Command;
 const Context = cli.Context;
 const Flag = cli.Flag;
