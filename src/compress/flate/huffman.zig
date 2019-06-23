e19e89fdace5b3a93506011de8d4352190355466
gernest <geofreyernest@live.com>
2019-06-23 17:00:25 +0300

    initial Huffman.bitCount implementation

--- src/compress/flate/huffman.zig
+++ src/compress/flate/huffman.zig
@@ -6,6 +6,7 @@ const reverseU16 = @import("bits.zig").reverseU16;
 
 pub const max_num_lit = 286;
 pub const max_bits_limit = 16;
+const max_i32 = math.maxInt(i32);
 
 pub var fixed_literal_encoding = &Huffman.generateFixedLiteralEncoding();
 pub var fixed_offset_encoding = &Huffman.generateFixedOffsetEncoding();
@@ -102,7 +103,7 @@ pub const Huffman = struct {
         return h;
     }
 
-    fn bitLength(self: *Huffman, freq: []i32) isize {
+    pub fn bitLength(self: *Huffman, freq: []i32) isize {
         var total: isize = 0;
         for (freq) |f, i| {
             if (f != 0) {
@@ -110,6 +111,77 @@ pub const Huffman = struct {
             }
         }
         return total;
+    }
+
+    pub fn bitCounts(self: *Huffman, list: LitaralNode, max_bits_arg: i32) []i32 {
+        var amx_bits = max_bits_arg;
+        assert(max_bits <= max_bits_limit);
+        const n = @intCast(i32, list.len);
+        var last_node = n + 1;
+        if (max_bits > n - 1) {
+            max_bits = n - 1;
+        }
+
+        var levels: [max_bits_limit]LevelInfo = undefined;
+        var leaf_counts: [max_bits_limit][max_bits_limit]i32 = undefined;
+
+        var level: i32 = 0;
+        while (level <= max_bits) : (level += 1) {
+            levels[@intCast(usize, level)] = LevelInfo{
+                .level = level,
+                .last_freq = list[1].freq,
+                .next_char_freq = list[2].freq,
+                .next_pair_freq = list[0].freq + list[1].freq,
+            };
+            leaf_counts[level][level] = 2;
+            if (level == 1) {
+                levels[@intCast(usize, level)].next_pair_freq = max_i32;
+            }
+        }
+        levels[max_bits].needed = 2 * n - 4;
+        level = max_bits;
+        while (true) {
+            var l = &levels[@intCast(usize, level)];
+            if (l.next_pair_freq == max_i32 and l.next_char_freq == max_i32) {
+                l.needed = 0;
+                levels[@intCast(usize, level + 1)].next_pair_freq = max_i32;
+                level += 1;
+                continue;
+            }
+            const prev_freq = l.last_freq;
+            if (l.next_char_freq < l.next_pair_freq) {
+                const nx = leaf_counts[level][level] + 1;
+                l.last_freq = l.next_char_freq;
+                leaf_counts[level][level] = nx;
+                l.next_char_freq = if (nx == last_node) LitaralNode.max().freq else list[nx].freq;
+            } else {
+                l.last_freq = l.next_pair_freq;
+                mem.copy(i32, leaf_counts[level][0..level], leaf_counts[level - 1][0..level]);
+                levels[l.level - 1].needed = 2;
+                l.needed -= 1;
+                if (l.needed == 0) {
+                    if (l.level == max_bits) {
+                        break;
+                    }
+                    levels[l.level + 1].next_pair_freq = prev_freq + l.last_freq;
+                    level += 1;
+                } else {
+                    while (level - 1 >= 0 and levels[level - 1].needed > 0) : (level -= 1) {}
+                }
+            }
+        }
+        if (leaf_counts[max_bits][max_bits] != n) {
+            @panic("leaf_counts[max_bits][max_bits] != n");
+        }
+        var bit_count = self.bit_count[0 .. max_bits + 1];
+        var bits = 1;
+        const counts = leaf_counts[max_bits];
+        level = max_bits;
+        while (level > 0) : (level -= 1) {
+            bit_count[bits] = counts[level] - counts[level - 1];
+            bits += 1;
+        }
+        return bit_count;
     }
 };
 
