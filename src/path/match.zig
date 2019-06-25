6b8ee73b07da92672e1414f741c09d46f1c09053
gernest <geofreyernest@live.com>
2019-06-26 00:15:39 +0300

    add docs for patch.match

--- src/path/match.zig
+++ src/path/match.zig
@@ -5,6 +5,28 @@ const warn = std.debug.warn;
 
 pub const MatchError = error{BadPattern};
 
+/// reports whether name matches the shell pattern.
+/// The pattern syntax is:
+///
+///  pattern:
+///    { term }
+///  term:
+///    '*'         matches any sequence of non-/ characters
+///    '?'         matches any single non-/ character
+///    '[' [ '^' ] { character-range } ']'
+///                character class (must be non-empty)
+///    c           matches character c (c != '*', '?', '\\', '[')
+///    '\\' c      matches character c
+///
+///  character-range:
+///    c           matches character c (c != '\\', '-', ']')
+///    '\\' c      matches character c
+///    lo '-' hi   matches character c for lo <= c <= hi
+///
+/// match requires pattern to match all of name, not just a substring.
+/// The only possible returned error is ErrBadPattern, when pattern
+/// is malformed.
+///
 pub fn match(pattern_string: []const u8, name_string: []const u8) MatchError!bool {
     var pattern = pattern_string;
     var name = name_string;
