bb756b8743aa755f228ded99259ef62a20d00b93
gernest <geofreyernest@live.com>
2019-06-29 12:39:55 +0300

    start work on regexp package

---
+++ src/regexp/syntax/syntax.zig
@@ -0,0 +1,149 @@
+const std = @import("std");
+const mem = std.mem;
+const Allocator = mem.Allocator;
+const ArrayList = std.ArrayList;
+
+pub const Context = struct {
+    /// returns arena allocator.
+    pub fn ar(self: *const Context) *Allocator {}
+
+    /// returns general allocator.
+    pub fn ga(self: *const Context) *Allocator {}
+};
+
+pub const FOLD_CASE: u16 = 1; // case-insensitive match
+pub const LITERAL: u16 = 2; // treat pattern as literal string
+pub const CLASS_NL: u16 = 4; // allow character classes like [^a-z] and [[:space:]] to match newline
+pub const DOT_NL: u16 = 8; // allow . to match newline
+pub const ONE_LINE: u16 = 16; // treat ^ and $ as only matching at beginning and end of text
+pub const NON_GREEDY: u16 = 32; // make repetition operators default to non-greedy
+pub const PERLX: u16 = 64; // allow Perl extensions
+pub const UNICODE_GROUPS: u16 = 128; // allow \p{Han}, \P{Han} for Unicode group and negation
+pub const WAS_DOLLAR: u16 = 256; // regexp OpEndText was $, not \z
+pub const SIMPLE: u16 = 512; // regexp contains no counted repetition
+
+pub const MATCH_NL = ClassNL | DotNL;
+
+pub const PERL = ClassNL | OneLine | PerlX | UnicodeGroups; // as close to Perl as possible
+pub const POSIX: u16 = 0; // POSIX syntax
+
+pub const Regexp = struct {
+    op: Op,
+    flags: u16,
+    sub: ?ArrayList(*Regexp),
+    rune: ?ArrayList(i32),
+    rune0: [2]i32,
+    min: isize,
+    max: isize,
+    cap: isize,
+    name: ?[]const u8,
+    ctx: *const Context,
+
+    pub const Op = enum {
+        NoMatch, // matches no strings
+        EmptyMatch, // matches empty string
+        Literal, // matches Runes sequence
+        CharClass, // matches Runes interpreted as range pair list
+        AnyCharNotNL, // matches any character except newline
+        AnyChar, // matches any character
+        BeginLine, // matches empty string at beginning of line
+        EndLine, // matches empty string at end of line
+        BeginText, // matches empty string at beginning of text
+        EndText, // matches empty string at end of text
+        WordBoundary, // matches word boundary `\b`
+        NoWordBoundary, // matches word non-boundary `\B`
+        Capture, // capturing subexpression with index Cap, optional name Name
+        Star, // matches Sub[0] zero or more times
+        Plus, // matches Sub[0] one or more times
+        Quest, // matches Sub[0] zero or one times
+        Repeat, // matches Sub[0] at least Min times, at most Max (Max == -1 is no limit)
+        Concat, // matches concatenation of Subs
+        Alternate, // matches alternation of Subs
+    };
+
+    pub fn init(ctx: *const Context) !Regexp {
+        return Regexp{
+            .op = .NoMatch,
+            .flags = 0,
+            .sub = null,
+            .rune = null,
+            .rune0 = [_]i32{ 0, 0 },
+            .min = 0,
+            .max = 0,
+            .cap = 0,
+            .name = null,
+            .ctx = ctx,
+        };
+    }
+
+    pub fn equal(x: *Regexp, y: *Regexp) bool {
+        if (x.op != y.op) {
+            return false;
+        }
+        switch (x.op) {
+            .EndText => {
+                if ((x.flags & WAS_DOLLAR) != (y.flags & WAS_DOLLAR)) {
+                    return false;
+                }
+            },
+            .Literal, .CharClass => {
+                if ((x.rune == null) != (y.rune == null)) {
+                    return false;
+                }
+                if (x.rune != null and y.rune != null) {
+                    if (x.rune.?.len != y.rune.?.len) {
+                        return false;
+                    }
+                    for (x.rune.?.toSlice()) |v, i| {
+                        if (v != y.rune.?.at(i)) {
+                            return false;
+                        }
+                    }
+                }
+            },
+            .Alternate, .Concat => {
+                if ((x.sub == null) != (y.sub == null)) {
+                    return false;
+                }
+                if (x.sub != null and y.sub != null) {
+                    if (x.rune.?.len != y.rune.?.len) {
+                        return false;
+                    }
+                    for (x.rune.?.toSlice()) |v, i| {
+                        if (v.equal(y.rune.?.at(i))) {
+                            return false;
+                        }
+                    }
+                }
+            },
+            .Star, .Plus, .Quest => {
+                if ((flags & NON_GREEDY) != (y.flags & NON_GREEDY) or !(x.sub.?.at(0).equal(y.sub.?.at(0)))) {
+                    return false;
+                }
+            },
+            .Repeat => {
+                if ((x.flags & NON_GREEDY) != (y.flags & NON_GREEDY) or
+                    (x.min != y.min) or x.max != y.max or !(x.sub.?.at(0).equal(y.sub.?.at(0))))
+                {
+                    return false;
+                }
+            },
+            .Capture => {
+                if ((x.cap != y.cap) or equalName(x, y) or !(x.sub.?.at(0).equal(x.sub.at(0)))) {
+                    return false;
+                }
+            },
+        }
+        return true;
+    }
+
+    fn equalName(x: *Regexp, y: *Regexp) bool {
+        if ((x.name == null) and (y.name == null)) {
+            return true;
+        }
+        if ((x.name == null) != (y.name == null)) {
+            return false;
+        }
+        return mem.eql(u8, x.name.?, y.name.?);
+    }
+};
