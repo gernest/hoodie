5d2c9d3a605e44e613f571848282fa7eaca38079
gernest <geofreyernest@live.com>
2019-07-07 00:22:57 +0300

    add comment

--- src/regexp/syntax/syntax.zig
+++ src/regexp/syntax/syntax.zig
@@ -170,6 +170,13 @@ pub const Regexp = struct {
         return mem.eql(u8, x.name.?, y.name.?);
     }
 
+    /// returns a regexp equivalent to re but without counted repetitions
+    /// and with various other simplifications, such as rewriting /(?:a+)+/ to /a+/.
+    /// The resulting regexp will execute correctly but its string representation
+    /// will not produce the same parse tree, because capturing parentheses
+    /// may have been duplicated or removed. For example, the simplified form
+    /// for /(x){1,2}/ is /(x)(x)?/ but both parentheses capture as $1.
+    /// The returned regexp may share structure with or be the original.
     pub fn simplify(re: *Regexp) !*Regexp {
         switch (re.op) {
             .Capture, .Concat, .Alternate => {
